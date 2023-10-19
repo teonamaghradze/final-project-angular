78dfaef0463ceba157e52679ab7c1e28e650bde5
Teona Maghradze <maghradzeteo@gmail.com>
2023-10-19 19:02:58 +0400

    add api for restaurants

--- final-project-angular/src/app/features/trip-plan/services/trip.service.ts
+++ final-project-angular/src/app/features/trip-plan/services/trip.service.ts
@@ -1,3 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TripService {
+  lat = 12.91285;
+  lon = 100.87808;
+
+  private apiBaseUrl = `https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${this.lat}&longitude=${this.lon}&limit=30&currency=USD&distance=2&open_now=false&lunit=km&lang=en_US`;
+
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<any> {
+    const headers = new HttpHeaders({
+      'X-RapidAPI-Key': '2be78c4877mshccdbbc203a80f0fp1ce79fjsne64cd595e355',
+      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+    });
+
+    return this.http.get(this.apiBaseUrl, { headers }).pipe(
+      catchError((error: any) => {
+        console.error('Error:', error);
+        return throwError(error);
+      })
+    );
+  }
+}
+
 // import { Injectable } from '@angular/core';
 // import { HttpClient, HttpHeaders } from '@angular/common/http';
 // import { Observable, throwError } from 'rxjs';
@@ -7,16 +38,24 @@
 //   providedIn: 'root',
 // })
 // export class TripService {
-//   private apiBaseUrl = '';
+//   private apiBaseUrl =
+//     'https://adsbx-flight-sim-traffic.p.rapidapi.com/api/aircraft/json/';
 
 //   constructor(private http: HttpClient) {}
 
-//   getData(): Observable<any> {
+//   getData(
+//     latitude: number,
+//     longitude: number,
+//     distance: number
+//   ): Observable<any> {
 //     const headers = new HttpHeaders({
-//       Authorization: 'BAvtwGolk1S9rNYyaWWrFscHQlhj',
+//       'X-RapidAPI-Key': '2be78c4877mshccdbbc203a80f0fp1ce79fjsne64cd595e355',
+//       'X-RapidAPI-Host': 'adsbx-flight-sim-traffic.p.rapidapi.com',
 //     });
 
-//     return this.http.get(this.apiBaseUrl, { headers }).pipe(
+//     const apiUrl = `${this.apiBaseUrl}lat/${latitude}/lon/${longitude}/dist/${distance}/`;
+
+//     return this.http.get(apiUrl, { headers }).pipe(
 //       catchError((error: any) => {
 //         console.error('Error:', error);
 //         return throwError(error);
--- final-project-angular/src/app/features/trip-plan/trip-list/trip-list.component.ts
+++ final-project-angular/src/app/features/trip-plan/trip-list/trip-list.component.ts
@@ -1,5 +1,38 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { Component, OnInit, OnDestroy } from '@angular/core';
 // import { TripService } from '../services/trip.service';
+
+// @Component({
+//   selector: 'app-trip-list',
+//   templateUrl: './trip-list.component.html',
+//   styleUrls: ['./trip-list.component.scss'],
+// })
+// export class TripListComponent implements OnInit {
+//   hotelData: any;
+
+//   constructor(private tripService: TripService) {}
+
+//   ngOnInit() {
+//     // this.tripService.getData().subscribe((data: any) => {
+//     //   this.hotelData = data;
+//     //   console.log(this.hotelData);
+//     //   console.log(data);
+//     // });
+
+//     const latitude = 40.7128;
+//     const longitude = -74.006;
+//     const distance = 25;
+
+//     this.tripService
+//       .getData(latitude, longitude, distance)
+//       .subscribe((data: any) => {
+//         this.hotelData = data;
+//         console.log(this.hotelData);
+//       });
+//   }
+// }
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TripService } from '../services/trip.service';
 
 @Component({
   selector: 'app-trip-list',
@@ -9,12 +42,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class TripListComponent implements OnInit {
   hotelData: any;
 
-  // constructor(private tripService: TripService) {}
+  constructor(private tripService: TripService) {}
 
   ngOnInit() {
-    // this.tripService.getData().subscribe((data: any) => {
-    //   this.hotelData = data;
-    //   console.log(this.hotelData);
-    // });
+    this.tripService.getData().subscribe((data: any) => {
+      this.hotelData = data;
+      console.log(this.hotelData);
+      console.log(data.data);
+    });
   }
 }
